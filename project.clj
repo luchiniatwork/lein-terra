d418ecd58e93b7aae20389d9f38546e53e32ea28
Tiago Luchini <luchini@work.co>
2017-09-16 17:06:34 -0400

    Update to `terra 0.1.2`

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject luchiniatwork/lein-terra "0.1.1"
+(defproject luchiniatwork/lein-terra "0.1.2"
   :description "Lein plugin for https://github.com/luchiniatwork/terra"
   :url "https://github.com/luchiniatwork/lein-terra"
   
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
 
-  :dependencies [[luchiniatwork/terra "0.1.1"]]
+  :dependencies [[luchiniatwork/terra "0.1.2"]]
   
   :eval-in-leiningen true)
